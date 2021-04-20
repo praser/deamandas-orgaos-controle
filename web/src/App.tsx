87a83df82f64addfc7d518bc4fed16a8f253c139
Rubens Praser Júnior <praser@gmail.com>
2021-04-20 16:07:34 -0300

    Criaçãod do componente VerticalRule

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import Profile from './components/organisms/Profile';
+import TopBar from './components/atoms/TopBar';
+import VerticalRule from './components/atoms/VerticalRule';
 
 function App(): JSX.Element {
   return (
-    <Profile
-      name="Rubens Praser Júnior"
-      lotation="GEOTR"
-      src="https://images.unsplash.com/photo-1554080353-a576cf803bda?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80"
-    />
+    <TopBar>
+      <VerticalRule />
+      <Profile
+        name="Rubens Praser Júnior"
+        lotation="GEOTR"
+        src="https://images.unsplash.com/photo-1554080353-a576cf803bda?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80"
+      />
+    </TopBar>
   );
 }
 
---
+++ web/src/components/atoms/VerticalRule.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const VerticalRule = styled.div`
+  /* Layout */
+  box-sizing: border-box;
+  display: block;
+  height: 38px;
+  margin-bottom: 16px;
+  margin-left: 16px;
+  margin-right: 16px;
+  margin-top: 16px;
+  width: 1px;
+
+  /* Appearance */
+  border-right-color: rgb(227, 230, 240);
+  border-right-style: solid;
+  border-right-width: 1px;
+`;
+
+export default VerticalRule;
